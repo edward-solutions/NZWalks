c88928a0f7898c9daa6471e64f305f725f65b750
edward cabanatan <edward.cabanatan@gmail.com>
2025-11-13 22:45:51 +0800

    Seed regions

--- NZWalks.API/Data/NZWalksDbContext.cs
+++ NZWalks.API/Data/NZWalksDbContext.cs
@@ -13,5 +13,48 @@ namespace NZWalks.API.Data
         public DbSet<Difficulty> Difficulties{ get; set; }
         public DbSet<Region> Regions{ get; set; }
         public DbSet<Walk> Walks{ get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Region>().HasData(
+                new Region
+                {
+                    Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                    Code = "AKL",
+                    Name = "Auckland Region",
+                    RegionImageUrl = "https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                    Code = "WGN",
+                    Name = "Wellington Region",
+                    RegionImageUrl = "https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("33333333-3333-3333-3333-333333333333"),
+                    Code = "BOP",
+                    Name = "Bay of Plenty",
+                    RegionImageUrl = "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("44444444-4444-4444-4444-444444444444"),
+                    Code = "NSN",
+                    Name = "Nelson Region",
+                    RegionImageUrl = "https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg"
+                },
+                new Region
+                {
+                    Id = Guid.Parse("55555555-5555-5555-5555-555555555555"),
+                    Code = "STL",
+                    Name = "Southland Region",
+                    RegionImageUrl = "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg"
+                }
+            );
+        }
     }
 }
---
+++ NZWalks.API/Migrations/20251113143716_SeedRegions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRegions : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Regions",
+                columns: new[] { "Id", "Code", "Name", "RegionImageUrl" },
+                values: new object[,]
+                {
+                    { new Guid("11111111-1111-1111-1111-111111111111"), "AKL", "Auckland Region", "https://images.pexels.com/photos/1054218/pexels-photo-1054218.jpeg" },
+                    { new Guid("22222222-2222-2222-2222-222222222222"), "WGN", "Wellington Region", "https://images.pexels.com/photos/1032650/pexels-photo-1032650.jpeg" },
+                    { new Guid("33333333-3333-3333-3333-333333333333"), "BOP", "Bay of Plenty", "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg" },
+                    { new Guid("44444444-4444-4444-4444-444444444444"), "NSN", "Nelson Region", "https://images.pexels.com/photos/46274/pexels-photo-46274.jpeg" },
+                    { new Guid("55555555-5555-5555-5555-555555555555"), "STL", "Southland Region", "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("11111111-1111-1111-1111-111111111111"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("22222222-2222-2222-2222-222222222222"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("33333333-3333-3333-3333-333333333333"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("44444444-4444-4444-4444-444444444444"));
+
+            migrationBuilder.DeleteData(
+                table: "Regions",
+                keyColumn: "Id",
+                keyValue: new Guid("55555555-5555-5555-5555-555555555555"));
+        }
+    }
+}
