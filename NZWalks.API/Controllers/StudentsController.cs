47725aabacd4f7aeb60fb5e4680df12f0dec1186
edward cabanatan <edward.cabanatan@gmail.com>
2025-11-13 21:16:13 +0800

    Add project files.

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "John", "Jane", "Mark", "David" };
+            return Ok(studentNames);
+        }
+    }
+}
